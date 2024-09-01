996dcb2eb11a4e9d9d80b05e6c52c78dc256caff
Thakshara Dhananjaya <thakshara@surge.global>
2024-09-02 01:16:44 +0530

    Fix: code format

--- utils/auth.ts
+++ utils/auth.ts
@@ -6,7 +6,6 @@ import type {
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-
 // Use it in server contexts
 export function auth(
   ...args:
