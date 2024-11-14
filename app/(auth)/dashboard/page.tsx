fdde1c8d4a57c98ee513fdf464db71cff3e801a3
Sanjaya Gayan <k.m.s.g.rathnayaka@gmail.com>
2024-11-14 22:21:27 +0530

    Feat: Update error fix

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,14 +1,5 @@
 import API from "constants/doctor";
 import DashboardPage1 from "modules/dashboard/DashboardPage1";
-import DashboardPage2 from "modules/dashboard/DashboardPage2";
-import DashboardPage3 from "modules/dashboard/DashboardPage3";
-import DashboardPage from "modules/dashboard/deplicated/DashboardPage";
-import MedicalInformationForm from "modules/medical-screening/MedicalInformationInForm";
-import PersonalInformationForm from "modules/medical-screening/PersonalInformationForm";
-import ProfileForm from "modules/medical-screening/ProfileForm";
-import QuestionMainSection from "modules/medical-screening/QuestionMainSection";
-import PatientDetail from "modules/Patients/PatientDetail";
-import PatientsCardList from "modules/Patients/PatientsCardList";
 import { notFound } from "next/navigation";
 import request from "utils/request";
 
@@ -16,7 +7,7 @@ export default async function page() {
   try {
     // const doctorDetails = await request(API.GET_DOCTORS, {}, false, false);
 
-    return <PatientDetail name={""} age={0} gender={""} languages={[]} screeningResult={""} dateOfBirth={""} salutation={""} lastAppointment={""} nextAppointment={""} email={""} weight={""} height={""} activityLevel={""} />;
+    return <DashboardPage1/>;
   } catch (e) {
     console.log(e);
     notFound();
